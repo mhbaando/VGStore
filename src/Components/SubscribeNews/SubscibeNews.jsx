e07792425dfc574ef26832e5427a8796084c94ed
Mohamud Abshir <maxamuudyare21@gmail.com>
2022-03-13 10:08:47 +0300

    Implemented Subscibre section and updated the variable gary and light-gray to match the color specified in the design

--- src/Components/SubscribeNews/SubscibeNews.jsx
+++ src/Components/SubscribeNews/SubscibeNews.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SubscibeNews = () => {
-  return <div>SubscibeNews</div>;
+  const [submited, setIsSubmitted] = useState("");
+  return (
+    <div className="vg__subscribeNews">
+      <h3>Subscribe For Our Newsletter</h3>
+      <h4>Stay Up To Date With Us</h4>
+      <div>
+        <input type="email" placeholder="e-mail" />
+        <div className="line"></div>
+        <button
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault();
+            setIsSubmitted("");
+          }}
+        >
+          Send
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default SubscibeNews;
--- src/Containers/Subscribe/Subscibe.jsx
+++ src/Containers/Subscribe/Subscibe.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import "./Subscribe.scss";
+import { SubscribeNews } from "../../Components/index";
+
 const Subscibe = () => {
-  return <div>Subscibe</div>;
+  return (
+    <div className="content-section">
+      <div className="content-container">
+        <SubscribeNews />
+      </div>
+    </div>
+  );
 };
 
 export default Subscibe;
